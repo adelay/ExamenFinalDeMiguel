87124adb72f79fdcf183d03f07e3323902f44ec7
adelay <miginiesta@gmail.com>
2013-03-05 12:28:50 +0000

    1 Version

---
+++ proyeMiguel2/src/persona.java
@@ -0,0 +1,56 @@
+
+/**
+ * Autor Miguel iniesta
+ * Colaborador Pablixo
+ */
+public class persona {
+	
+	String nombre;
+	String apellido;
+	int edad;
+	
+	/**
+	 * devuelve el nombre
+	 */
+	public String getNombre()
+	{
+		return nombre;
+	}
+	
+	/**
+	 * metodo que devuelve el apellido
+	 */
+	public String getApellido()
+	{
+		return apellido;
+	}
+	
+	/**
+	 * Metodo edad nuevo
+	 */
+	public int getEdad()
+	{
+		return edad;
+	}
+	
+	/**
+	 * Metodo edad
+	 */
+	public void setEdad(int edad2)
+	{
+		this.edad = edad2;
+	}
+	
+	/**
+	 * 
+	 */
+	public void setNombre(String nombre)
+	{
+		this.nombre = nombre;
+	}
+	
+	public void mostrar(){
+		System.out.println("Nombre" + nombre);
+	}
+	
+}
